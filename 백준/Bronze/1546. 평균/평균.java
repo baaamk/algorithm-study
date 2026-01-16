888f977d08bab48c9cf76b99659a3423d998c8f3
BBang <136908616+baaamk@users.noreply.github.com>
2026-01-16 20:49:48 +0900

    [Bronze I] Title: 평균, Time: 112 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,19 +1,36 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        long sum = 0;
-        long max = 0;
-
-        for (int i = 0; i < n; i++) {
-            int temp = scanner.nextInt();
-            if (temp > max) {
-                max = temp;
-            }
-            sum += temp;
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double[] list = new double[n];
+        for(int i = 0; i < list.length; i++){
+            //최대값 구하고
+            //모든 점수를 점수/M*100 로 변경
+            //그리고 평균 점수 
+            list[i] = (double) Integer.parseInt(st.nextToken());
         }
-        System.out.println(sum*100.0/max/n);
+        
+        double max = list[0];
+        
+        for(int i = 0; i < list.length; i++){
+            if(max <= list[i]){
+                max = list[i];
+            }
+        }
+        double result = 0;
+        for(int i = 0; i < list.length; i++){
+            list[i] = (list[i] / max) * 100;
+            result += list[i];
+        }
+        double average = result/list.length;
+        bw.write(String.valueOf(average));
+        bw.flush();
+        bw.close();
+        
     }
 }
