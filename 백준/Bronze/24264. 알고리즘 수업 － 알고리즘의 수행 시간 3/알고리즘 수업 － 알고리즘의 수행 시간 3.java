15d43240c054ab190acc8632ea02c04a12a8f712
BBang <136908616+baaamk@users.noreply.github.com>
2026-01-22 18:57:29 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long n = Long.parseLong(br.readLine());
+        bw.write(String.valueOf(n*n));
+        bw.newLine();
+        bw.write("2");
+        bw.flush();
+        bw.close();
+    }
+}
