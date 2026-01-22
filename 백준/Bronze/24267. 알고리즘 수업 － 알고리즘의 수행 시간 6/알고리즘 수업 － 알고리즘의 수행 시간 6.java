3b06c369386ecd513ea6d101b4c2e7ccb661871b
BBang <136908616+baaamk@users.noreply.github.com>
2026-01-22 19:30:23 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long n = Long.parseLong(br.readLine());
+        long result = n * (n - 1) * (n - 2) / 6;
+        bw.write(String.valueOf(result));
+        bw.newLine();
+        bw.write("3");
+        bw.flush();
+        bw.close();
+    }
+}
