58ad13c9e107017eb0b51885c2bc187459beaa12
BBang <136908616+baaamk@users.noreply.github.com>
2026-02-03 21:19:32 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15960 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] arr;
+    static int n;
+    static int m;
+    
+    static void create(int length, int start) throws IOException{
+        if(length == m){
+            for(int i = 0; i < m; i++){
+                bw.write(arr[i] + " ");
+            }
+            bw.newLine();
+            return;
+        }
+        for(int i = start; i <= n; i++){
+            arr[length] = i;
+            create(length + 1, i + 1);
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        create(0, 1);
+        bw.flush();
+        bw.close();
+        
+    }
+}
