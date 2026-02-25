79c75e8c1c4ffcb4fff758f82f6869d1b6c2719d
BBang <136908616+baaamk@users.noreply.github.com>
2026-02-25 18:20:59 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 124 ms, Memory: 16080 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -2,46 +2,37 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        String[] arr = new String[n];
-        String[] white = {"WBWBWBWB","BWBWBWBW"};
-        String[] black = {"BWBWBWBW","WBWBWBWB"};
-        
-        
+        String[] board = new String[n];
+        String[] white = {"WBWBWBWB", "BWBWBWBW"};
+        String[] black = {"BWBWBWBW", "WBWBWBWB"};
         for(int i = 0; i < n; i++){
-            arr[i] = br.readLine();
+            board[i] = br.readLine();
         }
-        
         int min = Integer.MAX_VALUE;
-        
         for(int i = 0; i < n - 7; i++){
             for(int j = 0; j < m - 7; j++){
-                int w = getCount(arr, i, j, white);
-                int b = getCount(arr, i, j, black);
-                
+                int w = getCount(board, i, j, white);
+                int b = getCount(board, i, j, black);
                 min = Math.min(min,Math.min(w,b));
-                
             }
         }
-        bw.write(min+"");
+        bw.write(String.valueOf(min));
         bw.flush();
         bw.close();
-        
     }
     
-    static int getCount(String[] arr, int startRow, int startCol, String[] pattern){
+    static int getCount(String[] board, int startRow, int startCol, String[] pattern){
         int count = 0;
-        
         for(int i = 0; i < 8; i++){
             for(int j = 0; j < 8; j++){
-                if(arr[startRow + i].charAt(startCol + j) != pattern[i % 2].charAt(j)){
+                if(board[startRow + i].charAt(startCol + j) != pattern[i % 2].charAt(j)){
                     count++;
                 }
             }
