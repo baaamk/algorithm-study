5d55128a18540b7c7c9d26f44508e6aea5ecbfcd
BBang <136908616+baaamk@users.noreply.github.com>
2026-03-05 21:15:57 +0900

    [Gold IV] Title: 빙산, Time: 592 ms, Memory: 252832 KB -BaekjoonHub

---
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -0,0 +1,112 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int n, m;
+    static int board[][];
+    static int temp[][];
+    static boolean visit[][];
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int year;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        
+        st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+        temp = new int[n][m];
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        while(true){
+        visit = new boolean[n][m];
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] != 0 && !visit[i][j]){
+                    bfsArea(i,j);
+                    count++;
+                }
+            }
+        }
+        
+        
+        if(count >= 2){
+            bw.write(year+"");
+            bw.flush();
+            bw.close();
+            return;
+        } else if(count == 0){
+            bw.write("0");
+            bw.flush();
+            bw.close();
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            temp[i] = board[i].clone();
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] != 0){
+                    int zero = 0;
+                    for(int k = 0; k < 4; k++){
+                        int nr = i + dr[k];
+                        int nc = j + dc[k];
+                        
+                        if(board[nr][nc] == 0){
+                            zero++;
+                        }
+                    }
+                    int newResult = board[i][j] - zero;
+                    if(newResult < 0){
+                        temp[i][j] = 0;
+                    } else {
+                        temp[i][j] = newResult;
+                    }
+                }
+                    
+                
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            board[i] = temp[i].clone();
+        }
+        year++;
+    }
+    
+    
+    }
+
+    
+    static void bfsArea(int r, int c){
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.add(new int[] {r,c});
+        visit[r][c] = true;
+        
+        while(!deque.isEmpty()){
+            int[] cur = deque.poll();
+            int cr = cur[0];
+            int cc = cur[1];
+            
+            for(int i = 0; i < 4; i++){
+                int nr = cr + dr[i];
+                int nc = cc + dc[i];
+                
+                if(board[nr][nc] != 0 && !visit[nr][nc]){
+                    visit[nr][nc] = true;
+                    deque.add(new int[]{nr,nc});
+                }
+            }
+        }
+    }
+}
