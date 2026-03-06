498ba54ff0c1842cbefadaedc9c11ef369aec5b5
BBang <136908616+baaamk@users.noreply.github.com>
2026-03-06 22:17:27 +0900

    [Silver II] Title: N과 M (9), Time: 256 ms, Memory: 26632 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    static int n,m;
+    static int[] arr;
+    static int[] select;
+    static boolean[] visit;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        visit = new boolean[n];
+        select = new int[m];
+        st = new StringTokenizer(br.readLine());
+        
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+        
+        make(0);
+        bw.flush();
+        bw.close();
+        
+        
+
+    }
+    
+    static void make(int depth) throws IOException {
+        if(depth == m){
+            for(int i = 0; i < m; i++){
+                bw.write(select[i] + " ");
+            }
+            bw.newLine();
+            return;
+        }
+        int prev = -1;
+        for(int i = 0; i < n; i++){
+            if(visit[i]){
+                continue;
+            }
+            if(prev == arr[i]){
+                continue;
+            }
+            select[depth] = arr[i];
+            visit[i] = true;
+            prev = arr[i];
+            make(depth+1);
+            visit[i] = false;
+        }
+    }
+  
+}
