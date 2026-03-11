b1d23fd81642c632274c4df030c51058381b379e
BBang <136908616+baaamk@users.noreply.github.com>
2026-03-11 18:39:31 +0900

    [Silver I] Title: 스타트와 링크, Time: 428 ms, Memory: 18328 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,69 +1,66 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    static int N;
-    static int[][] S;
+class Main{
+    static int n;
+    static int[][] board;
     static boolean[] visit;
     static int min = Integer.MAX_VALUE;
-
-    static void combi(int idx, int count) {
-        // 스타트팀 인원 N/2명 뽑으면 점수 계산
-        if (count == N / 2) {
-            calcDiff();
-            return;
-        }
-
-        for (int i = idx; i < N; i++) {
-            if (!visit[i]) {
-                visit[i] = true;          // i를 스타트팀으로 선택
-                combi(i + 1, count + 1);  // 다음 후보부터 계속 선택
-                visit[i] = false;         // 되돌리기
-            }
-        }
-    }
-
-    static void calcDiff() {
-        int start = 0;
-        int link = 0;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (visit[i] && visit[j]) {
-                    start = start + S[i][j] + S[j][i];
-                } else if (!visit[i] && !visit[j]) {
-                    link = link + S[i][j] + S[j][i];
-                }
-            }
-        }
-
-        int diff = Math.abs(start - link);
-        if (diff < min) {
-            min = diff;
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-
-        N = Integer.parseInt(br.readLine());
-        S = new int[N][N];
-        visit = new boolean[N];
-
-        for (int i = 0; i < N; i++) {
+        
+        n = Integer.parseInt(br.readLine());
+        
+        board = new int[n][n];
+        visit = new boolean[n];
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                S[i][j] = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < n; j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        combi(0, 0);
-
+        
+        dfs(0,0);
         bw.write(min + "");
-        bw.newLine();
         bw.flush();
         bw.close();
     }
+    
+    static void dfs(int index, int count){
+        if(count == n / 2){
+            calc();
+            return;
+        }
+        
+        for(int i = index; i < n; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                dfs(i + 1, count + 1);
+                visit[i] = false;
+            }
+        }
+    }
+    
+    static void calc(){
+        int start = 0;
+        int link = 0;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(visit[i] && visit[j]){
+                    start = start + board[i][j] + board[j][i];
+                } else if (!visit[i] && !visit[j]){
+                    link = link + board[i][j] + board[j][i];
+                }
+            }
+        }
+        
+        int result = Math.abs(start - link);
+        if(result < min){
+            min = result;
+        }
+    }
+    
 }
