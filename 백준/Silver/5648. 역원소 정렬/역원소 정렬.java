6dc1fae8405353891bada107bdaa4fd6b8db2c54
BBang <136908616+baaamk@users.noreply.github.com>
2026-03-12 19:44:28 +0900

    [Silver V] Title: 역원소 정렬, Time: 284 ms, Memory: 21400 KB -BaekjoonHub

---
+++ 백준/Silver/5648. 역원소 정렬/역원소 정렬.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        long[] arr = new long[n];
+
+        for(int i = 0; i < n; i++){
+            while(!st.hasMoreTokens()){
+                st = new StringTokenizer(br.readLine());
+            }
+
+            String num = st.nextToken();
+            String newA = "";
+
+            for(int j = num.length()-1; j >= 0; j--){
+                newA += num.charAt(j);
+            }
+
+            arr[i] = Long.parseLong(newA);
+        }
+
+        Arrays.sort(arr);
+
+        for(long a : arr){
+            bw.write(a + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
