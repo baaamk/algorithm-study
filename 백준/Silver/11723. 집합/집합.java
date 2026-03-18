399d03d5ac00f981d735c901d8f48e4c36531c27
BBang <136908616+baaamk@users.noreply.github.com>
2026-03-18 20:20:41 +0900

    [Silver V] Title: 집합, Time: 1100 ms, Memory: 298904 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,42 +1,33 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws IOException {
+class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int m = Integer.parseInt(br.readLine());
-        boolean[] arr = new boolean[21];
-
-        while (m-- > 0) {
+        int state = 0;
+        while(m -- >0){
             StringTokenizer st = new StringTokenizer(br.readLine());
             String com = st.nextToken();
-
-            if (com.equals("add")) {
+            if(com.equals("add")){
                 int x = Integer.parseInt(st.nextToken());
-                arr[x] = true;
-            } 
-            else if (com.equals("remove")) {
+                state |= (1 << (x - 1));
+            } else if(com.equals("remove")){
                 int x = Integer.parseInt(st.nextToken());
-                arr[x] = false;
-            } 
-            else if (com.equals("check")) {
+                state &= ~(1<<(x - 1));
+            } else if (com.equals("check")) {
                 int x = Integer.parseInt(st.nextToken());
-                bw.write((arr[x] ? 1 : 0) + "\n");
-            } 
-            else if (com.equals("toggle")) {
+                bw.write(((state >> (x - 1)) & 1) + "\n");
+            } else if (com.equals("toggle")) {
                 int x = Integer.parseInt(st.nextToken());
-                arr[x] = !arr[x];
-            } 
-            else if (com.equals("all")) {
-                Arrays.fill(arr, true);
-            } 
-            else if (com.equals("empty")) {
-                Arrays.fill(arr, false);
+                state ^= (1 << (x - 1));
+            } else if (com.equals("all")) {
+                state = (1 << 20) - 1;
+            } else if (com.equals("empty")) {
+                state = 0;
             }
         }
-
         bw.flush();
         bw.close();
     }
