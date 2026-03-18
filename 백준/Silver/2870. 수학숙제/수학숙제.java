f839ffe87fea2ba50008a3f7bbaa31d932e81052
BBang <136908616+baaamk@users.noreply.github.com>
2026-03-18 14:56:23 +0900

    [Silver IV] Title: 수학숙제, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.java
+++ 백준/Silver/2870. 수학숙제/수학숙제.java
@@ -1,56 +1,75 @@
 import java.io.*;
 import java.util.*;
 
-class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    static List<String> ans = new ArrayList<>();
+    static String s;
+
+    static void eraseChars(BufferedReader br) throws IOException {
+        s = br.readLine();
+        int pos = 0;
+        int nxtPos = 0;
+
+        while (true) {
+            while (nxtPos < s.length() && s.charAt(nxtPos) >= 'a' && s.charAt(nxtPos) <= 'z') {
+                nxtPos++;
+            }
+
+            pos = nxtPos;
+            if (pos >= s.length()) break;
+
+            while (nxtPos < s.length() && s.charAt(nxtPos) >= '0' && s.charAt(nxtPos) <= '9') {
+                nxtPos++;
+            }
+
+            ans.add(s.substring(pos, nxtPos));
+            pos = nxtPos;
+        }
+    }
+
+    static void eraseZeros() {
+        for (int i = 0; i < ans.size(); i++) {
+            String str = ans.get(i);
+            int pos = 0;
+
+            while (pos < str.length() && str.charAt(pos) == '0') {
+                pos++;
+            }
+
+            String ss = str.substring(pos);
+            if (ss.equals("")) ss = "0";
+
+            ans.set(i, ss);
+        }
+    }
+
+    static int cmp(String a, String b) {
+        if (a.length() == b.length()) {
+            return a.compareTo(b);
+        }
+        return a.length() - b.length();
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int n = Integer.parseInt(br.readLine());
-        List<String> list = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            String ans = "";
-            String a = br.readLine();
-            int j = 0;
-            while(j < a.length()){
-                if(a.charAt(j) >= '0' && a.charAt(j) <= '9'){
-                    ans+= String.valueOf(a.charAt(j));
-                } else {
-                    if(!ans.equals("")){
-                        list.add(removeZero(ans));
-                        ans = "";
-                    }
-                }
-                j++;
-            }
-            if(!ans.equals("")){
-                list.add(removeZero(ans));
-                ans = "";
-            }
-            
-            
-            
-            
-            
+
+        while (n-- > 0) {
+            eraseChars(br);
         }
-        list.sort((s1, s2) -> {
-            if(s1.length() != s2.length()){
-                return s1.length() - s2.length();
-            }
-            return s1.compareTo(s2);
-        });
-        for(String b : list){
-            bw.write(b + "");
+
+        eraseZeros();
+
+        ans.sort((a, b) -> cmp(a, b));
+
+        for (String str : ans) {
+            bw.write(str);
             bw.newLine();
         }
+
         bw.flush();
         bw.close();
-    }
-    
-    static String removeZero(String s){
-        int idx = 0;
-        while(idx < s.length() - 1 && s.charAt(idx) == '0'){
-            idx++;
-        }
-        return s.substring(idx);
     }
 }
