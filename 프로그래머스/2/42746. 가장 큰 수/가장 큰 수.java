3c5eb7ff4ca718c3677b754f53ec44b59867ef8f
방현우(Bang HyunWoo) <136908616+baaamk@users.noreply.github.com>
2026-03-26 15:59:46 +0900

    [level 2] Title: 가장 큰 수, Time: 233.81 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,20 +2,19 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-
-        StringBuilder sb = new StringBuilder();
         String[] arr = new String[numbers.length];
-        for(int i = 0; i < numbers.length; i++){
+        for(int i = 0; i <numbers.length; i++){
             arr[i] = String.valueOf(numbers[i]);
         }
-        
-        Arrays.sort(arr, (a,b) -> (b+a).compareTo(a + b));
+        Arrays.sort(arr, (a,b) -> (b+a).compareTo(a+b));
         if(arr[0].equals("0")){
             return "0";
         }
-        for(int i = 0; i < arr.length; i++){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < numbers.length; i++){
             sb.append(arr[i]);
         }
-        return sb.toString();
+        String answer = sb.toString();
+        return answer;
     }
 }
