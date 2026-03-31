af8524707644460e50801431d6baba77d5c42b5c
방현우(Bang HyunWoo) <136908616+baaamk@users.noreply.github.com>
2026-03-31 16:03:29 +0900

    [level 2] Title: 땅따먹기, Time: 23.89 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        
+        int n = land.length;
+        
+        for(int i = 1; i < n; i++){
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][1], Math.max(land[i-1][0], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][0]));
+        }
+        
+        int lastRow = n - 1;
+        return Math.max(Math.max(land[lastRow][0], land[lastRow][1]), 
+                        Math.max(land[lastRow][2], land[lastRow][3]));
+    
+    }
+}
